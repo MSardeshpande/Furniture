0fc80be9a9aade8a9537ccd363453f966d0bdaf4
MSardeshpande <82860526+MSardeshpande@users.noreply.github.com>
2022-05-14 10:41:38 +0530

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -6,36 +6,60 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
+    private HashMap<Furniture, Integer> orders;
 
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
+        orders = new HashMap<>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+        orders.put(type, furnitureCount);
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return orders;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+        float sum = 0f;
+        for(Map.Entry<Furniture, Integer> entries: orders) {
+            Furniture f = ordres.getKey();
+            sum+= cost;
+        }
+        return sum;
     }
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+        int c = 0;
+        for(Map.Entry<Furniture, Integer> entries: orders) {
+            Furniture f = ordres.getKey();
+            if(f.label().equalsIgnoreCase(type.label()) && f.cost() == type.cost()) {
+                c+=1;
+            }
+            
+        }
+        return c;
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+        float co = 0f;
+        for(Map.Entry<Furniture, Integer> entries: orders) {
+            Furniture f = ordres.getKey();
+            if(f.label().equalsIgnoreCase(type.label()) && f.cost() == type.cost()) {
+                co = f.cost();
+            }
+            
+        }
+        return co;
     }
 
     public int getTotalOrderQuantity() {
